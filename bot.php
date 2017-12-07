b9de8ace5b1ca6cc2f8019db5aef951db03ce390
thirdppp <third1025@hotmail.com>
2017-12-08 03:45:38 +0700

    s

--- bot.php
+++ bot.php
@@ -22,6 +22,15 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => $text
 			];
+      "messages":[
+       {
+           "type":"text",
+           "text":"Hello, user"
+       },
+       {
+            "type":"text",
+            "text":"May I help you?"
+        }
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
